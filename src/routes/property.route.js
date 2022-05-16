37a4c8e8389784d20bf4f7344fe9988ba4be4d54
davidessien925 <davidessien925@gmail.com>
2022-05-16 14:15:56 +0100

    user can view all property adverts

--- src/routes/property.route.js
+++ src/routes/property.route.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const propertyController = require("../controllers/property.controller")
 
-/*router.route('/properties')
-    .get(propertyController.findAll);*/
+router.route('/properties')
+    .get(propertyController.findAll);
 
 router.route('/properties')
     .post(propertyController.create);
